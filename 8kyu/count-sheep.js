41e321a292561837fab41dd6417528df065f1780
Mariya Tumelya <mtumelia@gmail.com>
2022-06-17 00:27:26 +0300

    feat: add the count-sheep solution

---
+++ 8kyu/count-sheep.js
@@ -0,0 +1,8 @@
+function countSheep(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+        arr.push(i + " sheep...");
+    }
+    return arr.join('');
+}
+console.log(countSheep(5));
